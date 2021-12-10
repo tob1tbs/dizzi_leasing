066dee43f25114e99068472aa4f7863b7cc13121
tob1tbs <chikhladze.mt@gmail.com>
2021-12-10 13:20:04 +0400

    revBl

--- app/Http/Controllers/CmsControllers/Reviews/ReviewsController.php
+++ app/Http/Controllers/CmsControllers/Reviews/ReviewsController.php
@@ -11,8 +11,15 @@ class ReviewsController extends Controller
     public function __construct() {
 
     }
-    
-    public function actionReviews(Request $Request) {
 
+    public function actionReviews(Request $Request) {
+        if (view()->exists('web.sections.review.review_main')) {
+
+            $data = [];
+
+            return view('web.sections.review.review_main', $data);
+        } else {
+            abort('404');
+        }
     }
 }
