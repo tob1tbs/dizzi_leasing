ad6dd055c682bcddb7a8c482aecb3ab63b6bba43
tob1tbs <chikhladze.mt@gmail.com>
2021-12-13 09:58:55 +0400

    Update about.blade.php

--- resources/views/web/sections/main/about.blade.php
+++ resources/views/web/sections/main/about.blade.php
@@ -39,5 +39,63 @@
             </div>
         </div>
     </section>
+    <section class="leadership-area pt-50 pb-50 bg_disable">
+        <div class="container">
+            <div class="row align-items-end">
+                <div class="col-sm-12">
+                    <div class="section-title  wow fadeInRight">
+                        <span class="short-title mt-0">Leadership</span>
+                        <h2 class="mb-0">Meet our leadership team</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row pt-40 gy-md-0 gy-4">
+                <div class="col-lg-3">
+                    <div class="single-leadership-widget wow fadeInUp " data-wow-delay="0.1s">
+                        <a href="#">
+                            <img src="{{ url('assets/web/img/leadership/img-1.png') }}" alt="leader-1">
+                            <div class="leader-info">
+                                <h5>Eldridge Robles</h5>
+                                <p>Co-Founder, Conis</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="single-leadership-widget wow fadeInUp " data-wow-delay="0.3s">
+                        <a href="#">
+                            <img src="{{ url('assets/web/img/leadership/img-2.png') }}" alt="leader-2">
+                            <div class="leader-info">
+                                <h5>Eldridge Robles</h5>
+                                <p>Co-Founder, Conis</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="single-leadership-widget wow fadeInUp " data-wow-delay="0.5s">
+                        <a href="#">
+                            <img src="{{ url('assets/web/img/leadership/img-3.png') }}" alt="leader-3">
+                            <div class="leader-info">
+                                <h5>Eldridge Robles</h5>
+                                <p>Co-Founder, Conis</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="single-leadership-widget wow fadeInUp " data-wow-delay="0.5s">
+                        <a href="#">
+                            <img src="{{ url('assets/web/img/leadership/img-3.png') }}" alt="leader-3">
+                            <div class="leader-info">
+                                <h5>Eldridge Robles</h5>
+                                <p>Co-Founder, Conis</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 @endsection
