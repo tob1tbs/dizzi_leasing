61e1a1b5956ce1bfe4e88bb224d6eb40b6965170
tob1tbs <chikhladze.mt@gmail.com>
2021-12-13 16:13:13 +0400

    Update layout.blade.php

--- resources/views/web/layout/layout.blade.php
+++ resources/views/web/layout/layout.blade.php
@@ -108,7 +108,19 @@
     <script type="text/javascript" src="{{ url('assets/web/js/wow.min.js') }}"></script>
     <script type="text/javascript" src="{{ url('assets/web/js/custom.js') }}"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <!-- <script src="//code.tidio.co/bclrzjv4kehofovksdqucovshaofwf6s.js" async></script> -->
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/61b731a580b2296cfdd16ecb/1fmppjs3v';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
     @yield('js')
 </body>
 </html>
