5838cff2f3586b1fd075035c30eb46b46954ad0d
tob1tbs <chikhladze.mt@gmail.com>
2021-12-13 18:38:03 +0400

    Update Blog.php

--- app/Models/Blogs/Blog.php
+++ app/Models/Blogs/Blog.php
@@ -11,7 +11,7 @@ class Blog extends Model
 
     protected $table = "cms_blogs";
 
-    protected $fillable = ['id'];
+    protected $fillable = ['id', 'title', 'text', 'photo', 'status', 'deleted_at_int', 'deleted_at'];
 
     public function blogCategory() {
         return $this->hasOne('App\Models\Blogs\BlogCategory', 'id', 'category_id');
