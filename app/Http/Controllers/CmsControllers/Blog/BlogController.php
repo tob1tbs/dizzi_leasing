4d0a8da7b5cc613eafaa984fce4d0c80fb4f8f59
tob1tbs <chikhladze.mt@gmail.com>
2021-12-13 17:51:28 +0400

    Update BlogController.php

--- app/Http/Controllers/CmsControllers/Blog/BlogController.php
+++ app/Http/Controllers/CmsControllers/Blog/BlogController.php
@@ -1,49 +1,39 @@
 <?php
 
-namespace App\Http\Controllers\WebControllers\Blogs;
+namespace App\Http\Controllers\CmsControllers\Blog;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Blogs\Blog;
 
-use Session;
-
 class BlogController extends Controller
 {
     //
 
-    public function actionWebBlog(Request $Request) {
-        if (view()->exists('web.sections.blog.blog_main')) {
+    public function actionBlog(Request $Request) {
+        if (view()->exists('cms.sections.blogs.blog_main')) {
 
             $Blog = new Blog();
-            $BlogList = $Blog::where('deleted_at_int', '!=', 0)->where('status', 1);
-
-            if($Request->has('search_query') && !empty($Request->search_query)) {
-                $BlogList->whereJsonContains('title', [Session::get('locale') => $Request->search_query]);
-            }
-
-            $BlogList = $BlogList->paginate(3)->load('blogCategory');
+            $BlogList = $Blog::where('deleted_at_int', '!=', 0)->get();
 
             $data = [
-                'blog_list' => $BlogList
+                'blog_list' => $BlogList,
             ];
-            return view('web.sections.blog.blog_main', $data);
+
+            return view('cms.sections.blogs.blog_main', $data);
         } else {
             abort('404');
         }
     }
 
-    public function actionWebBlogView(Request $Request) {
-        if (view()->exists('web.sections.blog.blog_view')) {
-
-            $Blog = new Blog();
-            $BlogData = $Blog::findOrFail($Request->id);
+    public function actionBlogAdd(Request $Request) {
+        if (view()->exists('cms.sections.blogs.blog_add')) {
 
             $data = [
-                'blog_data' => $BlogData
             ];
-            return view('web.sections.blog.blog_view', $data);
+
+            return view('cms.sections.blogs.blog_add', $data);
         } else {
             abort('404');
         }
