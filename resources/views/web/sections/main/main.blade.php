000f0fc6ac1c84aae1bed7b03a88744fbaf9d9d2
tob1tbs <chikhladze.mt@gmail.com>
2021-12-14 09:55:44 +0400

    Update main.blade.php

--- resources/views/web/sections/main/main.blade.php
+++ resources/views/web/sections/main/main.blade.php
@@ -4,7 +4,6 @@
 @php
     $vissible = 0;
 @endphp
-{{ $vissible }}
 <main>
     <section class="banner-area-3 pt-90" id="banner_animation2">
         <div class="bg-slides">
@@ -237,7 +236,7 @@
             </div>
         </div>
     </section>
-    @if($vissible != 1)
+    @if($vissible != 0)
     <section class="client-area pt-50 pb-100">
         <div class="container">
             <div class="section-title">
@@ -269,7 +268,7 @@
             <a class="theme-btn theme-btn-rounded-2 theme-btn-alt neue" href="javascript:;" onclick="OpenReviewModal()" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="margin: 20px auto 0; position: absolute; left: 50%; transform: translate(-50%, 0); background: #5651a1; color: #ffffff;">{{ trans('site.add_review') }}</a>
     </section>
     @endif
-    @if($vissible != 1)
+    @if($vissible != 0)
     <section class="testimonial-area pt-60 pb-60 bg_disable">
         <div class="container">
             <div class="section-title d-md-none mb-4">
@@ -317,7 +316,7 @@
         </div>
     </section>
     @endif
-    @if($vissible != 1)
+    @if($vissible != 0)
     <section class="news-area pb-60">
         <div class="container ">
             <div class="section-title pt-30">
