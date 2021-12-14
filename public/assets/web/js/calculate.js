f0f392de71776158c64fe8463020b342d827c77a
tob1tbs <chikhladze.mt@gmail.com>
2021-12-14 12:55:39 +0400

    Update calculate.js

--- public/assets/web/js/calculate.js
+++ public/assets/web/js/calculate.js
@@ -152,7 +152,7 @@ function Calculate(data) {
 	      inputFormat.value = values[handle];
 	      SelectedAmount = AmountFormat.from(values[handle]);
 	      mySliderPercent.noUiSlider.updateOptions({
-	      		start: [$("#PercetSetRangeAmount").val()],
+	      		start: [$("#SetRange").val() / 100 * $("#PercetSetRange")],
 			    range: {
 			        'min': $("#SetRange").val() * data['LeasingArray']['leasing_avanse_min_percent'] / 100,
 			        'max': $("#SetRange").val() * data['LeasingArray']['leasing_avanse_max_percent'] / 100,
