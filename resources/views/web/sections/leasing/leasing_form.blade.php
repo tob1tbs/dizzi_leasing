e1d708bc521d771b5c84c0d649b9c0c6a04bab29
tob1tbs <chikhladze.mt@gmail.com>
2021-12-16 18:05:26 +0400

    Update leasing_form.blade.php

--- resources/views/web/sections/leasing/leasing_form.blade.php
+++ resources/views/web/sections/leasing/leasing_form.blade.php
@@ -675,6 +675,7 @@
             mySliderPercent.noUiSlider.on("change", function (values, handle) {
                 $("#PercetSetRangeAmount").val(values[handle]);
                 $("#PercetSetRange").val((values[handle] / SelectedAmount * 100).toFixed(0));
+                calc();
             });
 
             mySliderPercent.noUiSlider.on("update", function (values, handle) {
