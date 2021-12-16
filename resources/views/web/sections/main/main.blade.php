7390b19ae268a3c085532419504906a665e35de2
tob1tbs <chikhladze.mt@gmail.com>
2021-12-16 17:06:41 +0400

    Update main.blade.php

--- resources/views/web/sections/main/main.blade.php
+++ resources/views/web/sections/main/main.blade.php
@@ -238,19 +238,19 @@
                     <div class="card-img mb-5 mb-sm-0">
                         <div class="shape-1">
                             <img class="wow fadeInLeft" data-wow-delay="1.2s" data-depth="0.2"
-                                src="{{ url('assets/web/img/banner/step_2.png') }}" alt="card" style="border-radius: 10px;">
+                                src="{{ url('assets/web/img/banner/step_2.png') }}" alt="card" style="border-radius: 10px; box-shadow: 0 0.5px 1px 0 rgb(51 77 114 / 10%), 0px 1.6px 3.6px 0 rgb(51 77 114 / 15%); transition: all 0.3s ease-out;">
                         </div>
                         <div class="shape-2">
                             <img class="wow fadeInRight" data-wow-delay="1s" data-depth="0.3"
-                                src="{{ url('assets/web/img/banner/step_1.png') }}" alt="card" style="border-radius: 10px;">
+                                src="{{ url('assets/web/img/banner/step_1.png') }}" alt="card" style="border-radius: 10px; box-shadow: 0 0.5px 1px 0 rgb(51 77 114 / 10%), 0px 1.6px 3.6px 0 rgb(51 77 114 / 15%); transition: all 0.3s ease-out;">
                         </div>
                         <div class="shape-3">
                             <img class="wow fadeInLeft" data-wow-delay="1.2s" data-depth="0.2" src="{{ url('assets/web/img/banner/step_3.png') }}"
-                                alt="card" style="border-radius: 10px;">
+                                alt="card" style="border-radius: 10px; box-shadow: 0 0.5px 1px 0 rgb(51 77 114 / 10%), 0px 1.6px 3.6px 0 rgb(51 77 114 / 15%); transition: all 0.3s ease-out;">
                         </div>
                         <div class="shape-4">
                             <img class="wow fadeInRight" data-wow-delay="1s" data-depth="0.3" src="{{ url('assets/web/img/blog/step.png') }}"
-                                alt="card" style="border-radius: 10px;">
+                                alt="card" style="border-radius: 10px; box-shadow: 0 0.5px 1px 0 rgb(51 77 114 / 10%), 0px 1.6px 3.6px 0 rgb(51 77 114 / 15%); transition: all 0.3s ease-out;">
                         </div>
                     </div>
                 </div>
