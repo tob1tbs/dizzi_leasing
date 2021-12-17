7933898acf26f25e67c491cb2db079a66b8ccacd
tob1tbs <chikhladze.mt@gmail.com>
2021-12-17 19:42:24 +0400

    Update header_1.blade.php

--- resources/views/web/include/header_1.blade.php
+++ resources/views/web/include/header_1.blade.php
@@ -41,7 +41,7 @@
 
 
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 <li class="nav-item">
 <a class="nav-link active" aria-current="page" href="#">Home</a>
 </li>
@@ -52,7 +52,7 @@
 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 Dropdown
 </a>
-<ul class="dropdown-menu mr-auto" aria-labelledby="navbarDropdown">
+<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 <li><a class="dropdown-item" href="#">Action</a></li>
 <li><a class="dropdown-item" href="#">Another action</a></li>
 <li><hr class="dropdown-divider"></li>
