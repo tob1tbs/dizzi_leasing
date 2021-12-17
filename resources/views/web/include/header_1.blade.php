9cfb4e5253dbe1819e0db2a8bb19bf4aeafe7634
tob1tbs <chikhladze.mt@gmail.com>
2021-12-17 19:40:48 +0400

    Update header_1.blade.php

--- resources/views/web/include/header_1.blade.php
+++ resources/views/web/include/header_1.blade.php
@@ -29,7 +29,7 @@
     <div class="container">
         <a class="navbar-brand sticky_logo" href="{{ route('actionWebMain') }}">
             <img class="main" src="{{ url('assets/web/img/logo/Logo_2.svg') }}" srcset="{{ url('assets/web/img/logo/Logo_2.svg') }} 2x" alt="logo" style="position: relative; left: -55px;">
-            <img class="sticky" src="{{ url('assets/web/img/logo/logo.svg') }}" srcset="{{ url('assets/web/img/logo/logo.svg') }} 2x" alt="logo" style="position: relative; left: -55px;">
+            <!-- <img class="sticky" src="{{ url('assets/web/img/logo/logo.svg') }}" srcset="{{ url('assets/web/img/logo/logo.svg') }} 2x" alt="logo" style="position: relative; left: -55px;"> -->
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -52,7 +52,7 @@
 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 Dropdown
 </a>
-<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+<ul class="dropdown-menu mr-auto" aria-labelledby="navbarDropdown">
 <li><a class="dropdown-item" href="#">Action</a></li>
 <li><a class="dropdown-item" href="#">Another action</a></li>
 <li><hr class="dropdown-divider"></li>
@@ -63,10 +63,6 @@ Dropdown
 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
 </li>
 </ul>
-<form class="d-flex">
-<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-<button class="btn btn-outline-success" type="submit">Search</button>
-</form>
 </div>
 </div>
 </nav>
