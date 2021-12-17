a6dcebf9b4a4be8789083acc4df063f67f217399
tob1tbs <chikhladze.mt@gmail.com>
2021-12-17 20:03:28 +0400

    Update header_1.blade.php

--- resources/views/web/include/header_1.blade.php
+++ resources/views/web/include/header_1.blade.php
@@ -26,45 +26,45 @@
     </div>
 </div>
 <div class="header-menu header-menu-4" id="sticky">
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-        <a class="navbar-brand sticky_logo" href="{{ route('actionWebMain') }}">
-            <img class="main" src="{{ url('assets/web/img/logo/Logo_2.svg') }}" srcset="{{ url('assets/web/img/logo/Logo_2.svg') }} 2x" alt="logo" style="max-width: 200px; width: 100%;position: relative; left: -55px;">
-            <img class="sticky" src="{{ url('assets/web/img/logo/logo.svg') }}" srcset="{{ url('assets/web/img/logo/logo.svg') }} 2x" alt="logo" style="max-width: 200px; width: 100%;position: relative; left: -55px;">
-        </a>
-        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="menu_toggle">
-                <span class="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+    <nav class="navbar navbar-expand-lg navbar-light" style="padding: 15px 0 !important;">
+        <div class="container">
+            <a class="navbar-brand sticky_logo" href="{{ route('actionWebMain') }}">
+                <img class="main" src="{{ url('assets/web/img/logo/Logo_2.svg') }}" srcset="{{ url('assets/web/img/logo/Logo_2.svg') }} 2x" alt="logo" style="max-width: 200px; width: 100%;position: relative; left: -55px;">
+                <img class="sticky" src="{{ url('assets/web/img/logo/logo.svg') }}" srcset="{{ url('assets/web/img/logo/logo.svg') }} 2x" alt="logo" style="max-width: 200px; width: 100%;position: relative; left: -55px;">
+            </a>
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="menu_toggle">
+                    <span class="hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                    <span class="hamburger-cross">
+                        <span></span>
+                        <span></span>
+                    </span>
                 </span>
-                <span class="hamburger-cross">
-                    <span></span>
-                    <span></span>
-                </span>
-            </span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a href="{{ route('actionWebMain') }}" class="nav-link">{{ trans('site.main') }}</a></li>
-                <li class="nav-item"><a href="{{ route('actionWebContact') }}" class="nav-link">{{ trans('site.our_office') }}</a></li>
-                <li class="nav-item"><a href="{{ route('actionWebRequsites') }}" class="nav-link">{{ trans('site.requisites') }}</a></li>
-                <li class="nav-item"><a href="{{ route('actionWebFaq') }}" class="nav-link">{{ trans('site.faq') }}</a></li>
-                <li class="nav-item"><a href="{{ route('actionWebAboutUs') }}" class="nav-link">{{ trans('site.about_us') }}</a></li>
-                <li class="nav-item dropdown submenu">
-                    <a class="theme-btn theme-btn-rounded-2 theme-btn-alt neue custom-button-color" href="#0" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 0 25px; font-size: 15px; position: relative; left: 15px;">{{ trans('site.get_loan') }}</a>
-                    <i class="arrow_carrot-down_alt2 mobile_dropdown_icon" aria-hidden="false" data-bs-toggle="dropdown"></i>
-                    <ul class="dropdown-menu">
-                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebLeasing') }}">{{ trans('site.leasing') }}</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebBackLeasing') }}">{{ trans('site.backleasing') }}</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebTaxiLeasing') }}">{{ trans('site.taxileasing') }}</a></li>
-                    </ul>
-                </li>
-            </ul>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a href="{{ route('actionWebMain') }}" class="nav-link">{{ trans('site.main') }}</a></li>
+                    <li class="nav-item"><a href="{{ route('actionWebContact') }}" class="nav-link">{{ trans('site.our_office') }}</a></li>
+                    <li class="nav-item"><a href="{{ route('actionWebRequsites') }}" class="nav-link">{{ trans('site.requisites') }}</a></li>
+                    <li class="nav-item"><a href="{{ route('actionWebFaq') }}" class="nav-link">{{ trans('site.faq') }}</a></li>
+                    <li class="nav-item"><a href="{{ route('actionWebAboutUs') }}" class="nav-link">{{ trans('site.about_us') }}</a></li>
+                    <li class="nav-item dropdown submenu">
+                        <a class="theme-btn theme-btn-rounded-2 theme-btn-alt neue custom-button-color" href="#0" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 0 25px; font-size: 15px; position: relative; left: 15px;">{{ trans('site.get_loan') }}</a>
+                        <i class="arrow_carrot-down_alt2 mobile_dropdown_icon" aria-hidden="false" data-bs-toggle="dropdown"></i>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="{{ route('actionWebLeasing') }}">{{ trans('site.leasing') }}</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ route('actionWebBackLeasing') }}">{{ trans('site.backleasing') }}</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ route('actionWebTaxiLeasing') }}">{{ trans('site.taxileasing') }}</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 </div>
 <div class="header-top2">
     <div class="container">
@@ -80,5 +80,13 @@
     </div>
 </div>
 <style type="text/css">
-    
+    .nav-link {
+        color:  #ffffff;
+        font-size: 20px;
+    }
+
+    .nav-link:hover {
+        color:  #f0c019;
+        font-size: 20px;
+    }
 </style>
