dadb88f6fdfaf2d5f15c240ff6caba5532be2790
tob1tbs <chikhladze.mt@gmail.com>
2021-12-17 20:00:39 +0400

    Update header_1.blade.php

--- resources/views/web/include/header_1.blade.php
+++ resources/views/web/include/header_1.blade.php
@@ -25,7 +25,8 @@
         </div>
     </div>
 </div>
-<nav class="navbar navbar-expand-lg navbar-light custom-bg-light" id="sticky">
+<div class="header-menu header-menu-4" id="sticky">
+<nav class="navbar navbar-expand-lg navbar-light custom-bg-light" >
     <div class="container">
         <a class="navbar-brand sticky_logo" href="{{ route('actionWebMain') }}">
             <img class="main" src="{{ url('assets/web/img/logo/Logo_2.svg') }}" srcset="{{ url('assets/web/img/logo/Logo_2.svg') }} 2x" alt="logo" style="max-width: 200px; width: 100%;position: relative; left: -55px;">
@@ -52,25 +53,19 @@
                 <li class="nav-item"><a href="{{ route('actionWebFaq') }}" class="nav-link">{{ trans('site.faq') }}</a></li>
                 <li class="nav-item"><a href="{{ route('actionWebAboutUs') }}" class="nav-link">{{ trans('site.about_us') }}</a></li>
                 <li class="nav-item dropdown submenu">
-                    <a class="theme-btn theme-btn-rounded-2 theme-btn-alt neue custom-button-color" href="#0" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 0 25px; font-size: 15px; position: relative; left: 15px;">
-                        {{ trans('site.get_loan') }}
-                    </a>
-                    <i class="arrow_carrot-down_alt2 mobile_dropdown_icon" aria-hidden="false"
-                        data-bs-toggle="dropdown"></i>
+                    <a class="theme-btn theme-btn-rounded-2 theme-btn-alt neue custom-button-color" href="#0" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 0 25px; font-size: 15px; position: relative; left: 15px;">{{ trans('site.get_loan') }}</a>
+                    <i class="arrow_carrot-down_alt2 mobile_dropdown_icon" aria-hidden="false" data-bs-toggle="dropdown"></i>
                     <ul class="dropdown-menu">
-                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebLeasing') }}">{{ trans('site.leasing') }}</a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebBackLeasing') }}">{{ trans('site.backleasing') }}</a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebTaxiLeasing') }}">{{ trans('site.taxileasing') }}</a>
-                        </li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebLeasing') }}">{{ trans('site.leasing') }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebBackLeasing') }}">{{ trans('site.backleasing') }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('actionWebTaxiLeasing') }}">{{ trans('site.taxileasing') }}</a></li>
                     </ul>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+</div>
 <div class="header-top2">
     <div class="container">
         <div class="d-xl-flex d-lg-flex d-md-flex justify-content-between">
