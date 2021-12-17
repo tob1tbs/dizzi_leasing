58b3e66870479a61c340e048b792c162b0bb0c58
tob1tbs <chikhladze.mt@gmail.com>
2021-12-17 19:17:17 +0400

    Update layout.blade.php

--- resources/views/web/layout/layout.blade.php
+++ resources/views/web/layout/layout.blade.php
@@ -117,10 +117,13 @@
     <script type="text/javascript" src="{{ url('assets/web/js/custom.js') }}"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!--Start of Tawk.to Script-->
-    <script>(function(a,m,o,c,r,m){a[m]={id:"100501",hash:"079e443732e2ba2422d0535c84f98fd51aa7c392e74aa6bf8bb095036f5a68cb",locale:"ru",inline:false,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1639649730';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));</script>
     
 
     <!--End of Tawk.to Script-->
     @yield('js')
+
+
+    <script>(function(a,m,o,c,r,m){a[m]={id:"100501",hash:"079e443732e2ba2422d0535c84f98fd51aa7c392e74aa6bf8bb095036f5a68cb",locale:"ru",inline:false,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1639649730';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));</script>
+    
 </body>
 </html>
