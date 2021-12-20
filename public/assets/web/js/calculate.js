fff03e424f57321650bfd4d52821c052b1f5aeb1
tob1tbs <chikhladze.mt@gmail.com>
2021-12-20 20:39:56 +0400

    Update calculate.js

--- public/assets/web/js/calculate.js
+++ public/assets/web/js/calculate.js
@@ -125,6 +125,8 @@ function Calculate(data) {
 	    //Slider Input Element
 	    var inputMonthFormat = document.getElementById("SetMonthRange");
 	    var inputFormat = document.getElementById("SetRange");
+		var inputPercentFormat = document.getElementById("PercetSetRange");
+
 
 	    SetPipsOnSlider(pips, mySlider);
 	    SetPipsOnSlider(pipsMonth, mySliderMonth);
@@ -223,6 +225,11 @@ function Calculate(data) {
 	      mySlider.noUiSlider.set(this.value);
 	      calc();
 	    });
+
+	    inputPercentFormat.addEventListener("change", function () {
+		  calc();
+		  mySliderPercent.noUiSlider.set($("#SetRange").val() / 100 * this.value);
+		});
 
 	    if ($("#monthTab.active").length > 0) {
 	      mySliderMonth.noUiSlider.on("update", function (values, handle) {
