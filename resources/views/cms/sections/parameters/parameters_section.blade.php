68409dce1e27a3d7bdd9bd5c7dab90c2822dfd1f
tob1tbs <chikhladze.mt@gmail.com>
2021-12-21 21:02:43 +0400

    Update parameters_section.blade.php

--- resources/views/cms/sections/parameters/parameters_section.blade.php
+++ resources/views/cms/sections/parameters/parameters_section.blade.php
@@ -3,5 +3,39 @@
 
 
 @section('content')
-@dd($sectionStatus['blog'][0])
+<div class="nk-block-head">
+    <div class="nk-block-between">
+        <div class="nk-block-head-content">
+            <h4 class="nk-block-title font-neue">სექციების გვერდების ჩამონათვალი</h4>
+        </div>  
+    </div>
+</div>
+<div class="card card-preview">
+    <div class="card-inner">
+        <table class="datatable-init nk-tb-list nk-tb-ulist font-helvetica-regular" data-auto-responsive="false">
+            <thead>
+                <tr class="nk-tb-item nk-tb-head font-helvetica-regular">
+                    <th class="nk-tb-col"><span class="sub-text">გვერდის დასახელება</span></th>
+                    <th class="nk-tb-col nk-tb-col-tools text-right"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($section_list as $section_item)
+                <tr class="nk-tb-item">
+                    <td class="nk-tb-col">
+                        <div class="user-card">
+                            <div class="user-info">
+                                <span class="tb-lead">{{ $section_item->name }}</span>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="nk-tb-col nk-tb-col-tools">
+                        
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
