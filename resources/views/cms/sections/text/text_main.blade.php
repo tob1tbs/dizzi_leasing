7c6f98be03db75ea29c9154ecbc7f00966c9c880
tob1tbs <chikhladze.mt@gmail.com>
2021-12-21 13:08:56 +0400

    Update text_main.blade.php

--- resources/views/cms/sections/text/text_main.blade.php
+++ resources/views/cms/sections/text/text_main.blade.php
@@ -12,6 +12,45 @@
         </div>  
     </div>
 </div>
+<div class="card card-preview">
+    <div class="card-inner">
+        <table class="datatable-init nk-tb-list nk-tb-ulist font-helvetica-regular" data-auto-responsive="false">
+            <thead>
+                <tr class="nk-tb-item nk-tb-head font-helvetica-regular">
+                    <th class="nk-tb-col"><span class="sub-text">გვერდის დასახელება</span></th>
+                    <th class="nk-tb-col nk-tb-col-tools text-right"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($text_page_data as $page_item)
+                <tr class="nk-tb-item">
+                    <td class="nk-tb-col">
+                        <div class="user-card">
+                            <div class="user-info">
+                                <span class="tb-lead">{{ $page_item->title }}</span>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="nk-tb-col nk-tb-col-tools">
+                        <ul class="nk-tb-actions gx-1">
+                            <li>
+                                <div class="drodown">
+                                    <a href="#" class="dropdown-toggle btn btn-icon btn-trigger" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <ul class="link-list-opt no-bdr" style="width: 300px;">
+                                            <li><a href="{{ route('actionTextPagesEdit', $page_item->id) }}"><em class="icon ni ni-edit"></em><span>რედაქტირება</span></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
 
 @section('js')
