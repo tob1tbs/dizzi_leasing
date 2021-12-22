7097831e88ca00fe9e23747f3195c9f414e2ec98
tob1tbs <chikhladze.mt@gmail.com>
2021-12-22 13:29:22 +0400

    Update blog_add.blade.php

--- resources/views/cms/sections/blogs/blog_add.blade.php
+++ resources/views/cms/sections/blogs/blog_add.blade.php
@@ -19,43 +19,36 @@
                 <div class="card card-preview">
                     <div class="card-inner">
                         <div class="row">
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-control-wrap mb-2">
                                     <label class="form-label" for="photo_main">ბლოგის დასახელება (ქართულად)</label>
                                     <input type="text" class="form-control font-neue" name="title_ge" id="title_ge">
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <!-- <div class="col-6">
                                 <div class="form-control-wrap mb-2">
                                     <label class="form-label" for="photo_main">ბლოგის დასახელება (ინგლისურად)</label>
                                     <input type="text" class="form-control font-neue" name="title_en" id="title_en">
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="col-12">
                                 <div class="form-control-wrap mb-2">
-                                     <div class="input-group">
-   <span class="input-group-btn">
-     <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-       <i class="fa fa-picture-o"></i> Choose
-     </a>
-   </span>
-   <input id="thumbnail" class="form-control" type="text" name="filepath">
- </div>
- <img id="holder" style="margin-top:15px;max-height:100px;">
+                                    <label class="form-label" for="photo_main">ბლოგის სურათი</label>
+                                    <input type="file" class="form-control font-neue" name="photo" id="photo">
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-control-wrap mb-2">
                                     <label class="form-label" for="photo_main">ბლოგის ტექსტი (ქართულად)</label>
                                     <textarea class="summernote" name="text_ge"></textarea>
                                 </div>
-                            </div>
+                            </div><!-- 
                             <div class="col-6">
                                 <div class="form-control-wrap mb-2">
                                     <label class="form-label" for="photo_main">ბლოგის ტექსტი (ინგლისურად)</label>
                                     <textarea class="summernote" name="text_en"></textarea>
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="col-6">
                                 <div class="form-control-wrap mb-2">
                                     <button class="btn btn-success font-neue" type="button" onclick="BlogAddSubmit()">შენახვა</button>
