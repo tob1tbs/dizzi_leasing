30b1f60f2094e20318bb02943003fb96029ac8a1
tob1tbs <chikhladze.mt@gmail.com>
2021-12-22 12:49:37 +0400

    Update layout.blade.php

--- resources/views/web/layout/layout.blade.php
+++ resources/views/web/layout/layout.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="facebook-domain-verification" content="f35co0iz84xewjylp4gw1a7du8fb5k" />
 
     <meta name="keywords" content="@if(!empty($seo_data['keywords_ge'])) @else {{ $parameterItems['keywords_ge'][0] }} @endif">
     <meta name="descrition" content="@if(!empty($seo_data['description_ge'])) @else {{ $parameterItems['description_ge'][0] }} @endif">
